e792529d4b66adca00a5a5f70e0b04887a010d74
Roberto-Cavalcanti <103041610+Roberto-Cavalcanti@users.noreply.github.com>
2022-10-18 20:34:36 -0300

    1° aula de JavaScript

---
+++ aula-de-JS/index.js
@@ -0,0 +1,17 @@
+/* console.log("Funcionou!") */
+/* const nomeUsuario = "John";
+let sobrenomeUsuario = "Carrazzoni";
+var idadeUsuario = 19;
+
+console.log("Olá, meu nome é ",nomeUsuario,"e meu sobrenome é ", sobrenomeUsuario, " e tenho", idadeUsuario," anos"); */
+
+//atividade 2
+
+/* const arrayNumbers = [1, 2, 3, 4, 5, 6]
+
+console.log(arrayNumbers[0], arrayNumbers[1], arrayNumbers[2])
+console.log(arrayNumbers[0]*2, arrayNumbers[1]*2, arrayNumbers[2]*2)
+console.log(typeof arrayNumbers[0]) */
+
+
+
