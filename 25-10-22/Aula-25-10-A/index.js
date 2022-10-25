cdeb88fbb42b4b33e8ca6f53d57db14fe2d38cd0
Roberto-Cavalcanti <103041610+Roberto-Cavalcanti@users.noreply.github.com>
2022-10-25 20:39:51 -0300

    Add files via upload

---
+++ 25-10-22/Aula-25-10-A/index.js
@@ -0,0 +1,21 @@
+/* console.log("Funciona!") */
+
+const variavelNumero01 = 9;
+const variavelNumero02 = -9;
+
+console.log(
+    "Primeira é igual a segunda?",
+variavelNumero01==variavelNumero02)
+
+console.log(
+    "Primeira é diferente a segunda?",
+variavelNumero01!==variavelNumero02)
+
+console.log(
+    "Primeira é maior que a segunda?",
+variavelNumero01>variavelNumero02)
+
+
+console.log(
+    "Primeira é menor que a segunda?",
+variavelNumero01<variavelNumero02)
---
+++ 25-10-22/Aula-25-10-B/index.js
@@ -0,0 +1,28 @@
+/* const a = true;
+const b = false;
+const c = true;
+
+console.log("", "a && b", a && b);
+console.log("", "b && c", b && c);
+console.log("", "a && b && c", a && b && c);
+
+
+console.log("", "a || b", a || b);
+console.log("", "b || c", b || c);
+console.log("", "a || b || c", a || b || c);
+ */
+
+const array = [1,2,3,4,5,6];
+
+console.log(array.length);
+
+array.push(7);
+
+console.log(array);
+
+array.splice(3,3);
+
+console.log(array);
+
+console.log(array.length);
+
